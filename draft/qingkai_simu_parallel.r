1e325162727902c56f640212249a6bb0fe6a5d30
QingkaiDong <dongqingkai@126.com>
2022-12-03 23:53:51 +0800

    add some code for plot

--- draft/qingkai_simu_parallel.r
+++ draft/qingkai_simu_parallel.r
@@ -3,7 +3,7 @@
 # If you want to get the results of all methods and all criteria for certain
 # full-data at once, please use this code.
 
-# Please load all function(incluing 'subsampling_simu' in this code),
+# Please load all function(including 'subsampling_simu' in this code),
 # and choose a type of data to generate,
 # and then run the simu code(line 138 in this code).
 
@@ -56,10 +56,10 @@ N <- 1e4
 beta0  <- c(rep(0.5, 7))
 d <- length(beta0)
 corr  <- 0.5
-sigmax  <- matrix(corr, d-1, d-1) + diag(1-corr, d-1)
+sigmax  <- matrix(corr, d, d) + diag(1-corr, d)
 set.seed(123)
-X <- rmvnorm(N, rep(1.5, d-1), sigmax)
-X <- cbind(1, X)
+X <- rmvnorm(N, rep(1.5, d), sigmax)
+#X <- cbind(1, X)
 P <- 1 - 1 / (1 + exp(X %*% beta0))
 y <- rbinom(N, 1, P)
 #table(y)
@@ -71,8 +71,8 @@ title <- 'simu3 hetero multinormal'
 N <- 1e4
 beta0  <- c(rep(0.5, 7))
 d <- length(beta0)
-var <- 1/c(1:(d))^2 #
-#var <- c(1:(d-1)) #^2
+#var <- 1/c(1:d)^2 #
+var <- c(1:(d)) #^2
 corr <- 0.5
 sigmax <- matrix(corr, d, d)  + diag(as.vector(var)) - diag(corr, d)
 set.seed(123)
@@ -94,10 +94,10 @@ N <- 1e4
 beta0  <- c(rep(0.5, 7))
 d <- length(beta0)
 corr  <- 0.5
-sigmax  <- matrix(corr, d-1, d-1) + diag(1-corr, d-1)
+sigmax  <- matrix(corr, d, d) + diag(1-corr, d)
 set.seed(123)
-X <- 0.5*rmvnorm(N, rep(1, d-1), sigmax) + 0.5*rmvnorm(N, rep(-1, d-1), sigmax)
-X <- cbind(1, X)
+X <- 0.5*rmvnorm(N, rep(1, d), sigmax) + 0.5*rmvnorm(N, rep(-1, d), sigmax)
+# X <- cbind(1, X)
 P <- 1 - 1 / (1 + exp(X %*% beta0))
 y <- rbinom(N, 1, P)
 table(y)
@@ -110,9 +110,9 @@ N <- 1e4
 beta0  <- c(rep(0.5, 7))
 d <- length(beta0)
 corr  <- 0.5
-sigmax  <- matrix(corr, d-1, d-1) + diag(1-corr, d-1)
+sigmax  <- matrix(corr, d, d) + diag(1-corr, d)
 set.seed(123)
-X <-rmvt(N, sigmax, df=3, rep(0, d-1), type=c("shifted","Kshirsagar")[1])#in JASA2018, it is "X/10". maybe that is a typo.
+X <-rmvt(N, sigmax, df=3, rep(0, d), type=c("shifted","Kshirsagar")[1])#in JASA2018, it is "X/10". maybe that is a typo.
 X <- cbind(1, X)
 P <- 1 - 1 / (1 + exp(X %*% beta0))
 y <- rbinom(N, 1, P)
@@ -126,10 +126,10 @@ N <- 1e4
 beta0  <- c(rep(0.5, 7))
 d <- length(beta0)
 set.seed(123)
-X <- matrix(0, N, d-1)
+X <- matrix(0, N, d)
 generate_rexp <- function(x) x<-rexp(N, rate=2)
 X <- apply(X, 2, generate_rexp)
-X <- cbind(1, X)
+#X <- cbind(1, X)
 P <- 1 - 1 / (1 + exp(X %*% beta0))
 y <- rbinom(N, 1, P)
 table(y)
@@ -142,10 +142,9 @@ r0 <- 200
 r_min <- 200
 r_max <- 1000
 num_r <- c(100, seq(r_min, r_max, 200)) # different r to be considered
-beta_temp <- matrix(0, S, d)
-weight_parameter = c(F, T)
-criteria_all = c("optA", "optL", "LCC")
-method_all = c("SWR", "Poisson")
+weight_parameter <-  c(F, T)
+criteria_all <- c("optA", "optL", "LCC")
+method_all <- c("SWR", "Poisson")
 mse <- c()
 cl <- makeCluster(length(num_r)) # how many CPU cores are called
 t1=proc.time()
@@ -162,7 +161,7 @@ for (w in 1:length(weight_parameter)){
                     envir=environment()) #import environment variables into the function 'subsampling_simu'
       results <- parLapply(cl, 1:length(num_r), subsampling_simu) #only output the mse
       for(i in 1:length(num_r)){
-        mse_criteria[q,i] <- results[[i]] #
+        mse_criteria[q,i] <- results[[i]]
       }
       t2=proc.time()
       time.cost=t2-t1
@@ -173,13 +172,42 @@ for (w in 1:length(weight_parameter)){
   }
 }
 mse_dataframe <- as.data.frame(mse,
-                               row.names=c('we_SWR_optA', 'we_SWR_optL', 'we_SWR_LCC',
+                               row.names = c('we_SWR_optA', 'we_SWR_optL', 'we_SWR_LCC',
                                            'we_Poi_optA', 'we_Poi_optL', 'we_Poi_LCC',
                                            'unwe_SWR_optA', 'unwe_SWR_optL', 'unwe_SWR_LCC',
-                                           'unwe_Poi_optA', 'unwe_Poi_optL', 'unwe_Poi_LCC')
-) # row names are different method
-colnames(mse_dataframe) <- as.character(num_r) # col names are different r to be considered
-print(title) #print current simu data type
-mse_dataframe
+                                           'unwe_Poi_optA', 'unwe_Poi_optL', 'unwe_Poi_LCC'))
+colnames(mse_dataframe) <- as.character(num_r)
+# row names are different method, col names are different r to be considered
+print(title) # print current simu data type
+#mse_dataframe
 #log(mse_dataframe)
 
+####  plot  ##################
+library(ggplot2)
+library(reshape2)
+mse_plot <- data.frame(r = num_r
+                       , we_SWR_optA = mse[1,]
+                       , we_SWR_optL = mse[2,]
+                       , we_SWR_LCC = mse[3,]
+
+                       , we_Poi_optA = mse[4,]
+                       , we_Poi_optL = mse[5,]
+                       , we_Poi_LCC = mse[6,]
+
+                       , unwe_SWR_optA = mse[7,]
+                       , unwe_SWR_optL = mse[8,]
+                       , unwe_SWR_LCC = mse[9,]
+
+                       , unwe_Poi_optA = mse[10,]
+                       , unwe_Poi_optL = mse[11,]
+                       , unwe_Poi_LCC = mse[12,]
+                       )
+# If you don't want to draw so many methods on the same plot,
+# you can comment out certain lines.
+mse_plot<-melt(mse_plot, id.vars = 'r', variable.name="method",
+  value.name="mse") # convert data structure to fit ggplot
+# mse_plot$mse <- log(mse_plot$mse) # take log(mse)
+mse_figure <- ggplot(mse_plot, aes(x=r, y=mse)) +
+  geom_line(aes(colour = method)) + geom_point() +
+  scale_x_continuous(breaks = num_r)
+mse_figure
